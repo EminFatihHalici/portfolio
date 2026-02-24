17c1961966e5e035a32bfc75c6398b83d1f6f756
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-02-24 13:24:05 +0100

    fix: correct variable declaration in openProjects function and adjust h3 padding for better styling

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function init() {
-  openProjects(null, "join");
   document.getElementById("defaultOpen").click();
 }
 
 function openProjects(evt, projectName) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("project-info-section");
+  let tabcontent = document.getElementsByClassName("project-info-section");
   for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("project-name");
+  let = tablinks = document.getElementsByClassName("project-name");
   for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
   }
