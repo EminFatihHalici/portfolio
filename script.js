bd80b448c54e2ae0198a4ba85b78240566f58051
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-02-24 13:00:01 +0100

    refactor: update openProjects function to display project info as flex and manage active states conditionally

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ function init() {
 
 function openProjects(evt, projectName) {
   let i, tabcontent, tablinks;
-
   tabcontent = document.getElementsByClassName("project-info-section");
   for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
   tablinks = document.getElementsByClassName("project-name");
   for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
   }
-
-  document.getElementById(projectName).style.display = "block";
-  evt.currentTarget.classList.add("active");
+  document.getElementById(projectName).style.display = "flex";
+  if (evt) {
+    evt.currentTarget.classList.add("active");
+  } else {
+    document.getElementById("defaultOpen").classList.add("active");
+  }
 }
