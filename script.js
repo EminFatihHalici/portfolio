c47f2c2df2fd922c301437469903f9a9a860f102
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-02-24 12:39:08 +0100

    feat: enhance project tab functionality with dynamic button actions and improve CSS styles

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+function init() {
+  openProjects(null, "join");
+  document.getElementById("defaultOpen").click();
+}
+
 function openProjects(evt, projectName) {
   let i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("project-info-section");
-  for (i = 0, i < tabcontent.length; i++; ) {
+  for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   tablinks = document.getElementsByClassName("project-name");
   for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].classList.remove("active");
   }
 
   document.getElementById(projectName).style.display = "block";
-  evt.currentTarget.className += "active";
+  evt.currentTarget.classList.add("active");
 }
